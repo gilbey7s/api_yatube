5aca7edac2a5762e294dfef72dda8dbdb00afd0f
gilbey7s <s.l.a.y.e.r.energy@yandex.ru>
2021-12-04 18:46:43 +0300

    for review

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from posts.models import Comment, Group, Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'text', 'pub_date', 'author', 'group',)
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description',)
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(read_only=True,
+                                          slug_field='username')
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'author', 'post', 'text', 'created',)
+        read_only_fields = ('post',)
--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -3,8 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+# from api import urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include('api.urls'))
 ]
 
 
