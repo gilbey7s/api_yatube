3c51b2f71acb1fe49f9ac280539dc0f3b64978bb
gilbey7s <s.l.a.y.e.r.energy@yandex.ru>
2021-12-07 13:12:25 +0300

    review

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -3,11 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-# from api import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls'))
+    path('api/', include('api.urls'))
 ]
 
 
