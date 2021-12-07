608008e0dfbd9bd023de5c82290f4d062403e051
gilbey7s <s.l.a.y.e.r.energy@yandex.ru>
2021-12-07 13:15:05 +0300

    review

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -8,11 +8,10 @@ router_v1 = DefaultRouter()
 router_v1.register('posts', PostViewSet)
 router_v1.register('groups', GroupViewSet)
 router_v1.register(r'posts/(?P<post_id>\d+)/comments',
-                CommentViewSet, basename='comments')
+                   CommentViewSet, basename='comments')
 
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
     path('v1/api-token-auth/', views.obtain_auth_token),
-    
 ]
